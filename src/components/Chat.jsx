85b5f9759ceb711371c07c337005bf0838c9c75b
bindu-19-code <bindukreddy1111@gmail.com>
2025-03-19 00:14:44 +0530

    Updated feature: [package.json]

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { FaSearch } from "react-icons/fa";
+import { IoSend } from "react-icons/io5";
 import "../App.css";
 
 const ChatPage = () => {
